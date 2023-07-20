60aa8220f5f03b9f2f661f87878fc95a0deee6c6
NickError404 <nicklink.lv@gmail.com>
2023-07-20 14:48:36 -0300

    update

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CreateUserDto } from './create-user.dto';
 import { UpdateUserDto } from './update-user.dto';
 import { User } from './user.entity';
